7ef14d8dc6387d30f76cdd3ba85e72439dc45fbf
Cloudiu9 <claudiubordea5@gmail.com>
2026-03-31 18:01:14 +0300

    Made cards and background random.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CardContainer from "./components/CardContainer";
 
 export default function App() {
+  const [bgImage, setbgImage] = useState("");
+
   const BACKGROUND_URL =
     "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
 
-  const fetchBackground = async () => {
-    // right now, fetches static Aatrox_1 background img
-    // need to fetch random champ and random number (1-4)
-    const image = BACKGROUND_URL + "Aatrox_1.jpg";
-
-    const appCon = document.querySelector("#App");
-    console.log(appCon);
-
-    appCon.style.backgroundImage = `url(${image})`;
-  };
+  const API_URL = "https://ddragon.leagueoflegends.com/cdn/16.1.1/";
 
   useEffect(() => {
+    let isActive = true; // Cleanup flag
+    const fetchBackground = async () => {
+      // Fetching all champs
+      const res = await fetch(API_URL + "data/en_US/champion.json");
+      const data = await res.json();
+      const allChamps = Object.keys(data.data);
+
+      // Getting a random champion
+      const randomName =
+        allChamps[Math.floor(Math.random() * allChamps.length)];
+      // Making a random number 1-4 (assuming all champs have at least 4 skins)
+      const randomNum = Math.floor(Math.random() * 4) || 1;
+      console.log(randomNum);
+
+      // Make default something safe in case champ younger than Zoe (by release date)
+      const image = BACKGROUND_URL + `${randomName}_${randomNum}.jpg`;
+      const fallbackImage = BACKGROUND_URL + "Aatrox_0.jpg";
+
+      const img = new Image();
+      img.src = image;
+
+      // used help for this
+      img.onload = () => {
+        if (isActive) setbgImage(image); // Only update if this effect is still "fresh"
+      };
+      img.onerror = () => {
+        if (isActive) setbgImage(fallbackImage);
+      };
+    };
+
     fetchBackground();
+
+    return () => {
+      isActive = false;
+    }; // "Kill" the previous effect run
   }, []);
 
   return (
     <div
       style={{
         height: 100 + "vh",
+        backgroundImage: `url(${bgImage})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
+        transition: "background-image 0.5s ease-in-out",
       }}
       id="App"
     >
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,31 +1,61 @@
 // will need to change amount of card dynamically (difficulty), probably state passed down from parent?
 
+// PASS DOWN IMAGE AND TITLE API TO EACH CARD? USEEFFECT?
+// images need to remain 'static' (don't change ALL images after one click)
+// https://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json
+// https://developer.riotgames.com/docs/lol
+
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 export default function CardContainer() {
-  // PASS DOWN IMAGE AND TITLE API TO EACH CARD? USEEFFECT?
-  // images need to remain 'static' (don't change ALL images after one click)
-  // https://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json
-  // https://developer.riotgames.com/docs/lol
+  const API_URL = "https://ddragon.leagueoflegends.com/cdn/16.1.1/";
+
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    // get random champ portrait + name
+    const loadData = async () => {
+      // fetching URL with info about all champs
+      const res = await fetch(API_URL + "data/en_US/champion.json");
+      const data = await res.json();
+
+      // Getting a random champion
+      const allChamps = Object.keys(data.data);
+
+      // used some help for this
+      // Need a Set so there's no duplicate champs
+      const uniqueChamps = new Set();
+      for (let i = 0; uniqueChamps.size < 12; i++) {
+        const randomName =
+          allChamps[Math.floor(Math.random() * allChamps.length)];
+        uniqueChamps.add(data.data[randomName]);
+      }
+      setCards(uniqueChamps);
+    };
+
+    loadData();
+  }, []); // only on page load
 
   return (
     <>
-      {/* scale/change padding depending on difficulty */}
-      <h1 style={{ padding: 50 + "px" }}>Memory Card Game</h1>
+      {/* scale/change padding depending on difficulty? */}
+      <h1 style={{ padding: 20 + "px", paddingBottom: 10 + "px" }}>
+        Memory Card Game
+      </h1>
+      <p style={{ padding: 20 + "px", fontSize: 20 + "px", color: "tomato" }}>
+        Don't click the same champion!
+      </p>
       <div id="flexCon">
         <div id="cardContainer">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {/* used some help for this */}
+          {/* spreading Set back into array */}
+          {[...cards].map((champ) => (
+            <Card
+              key={champ.id}
+              name={champ.name}
+              image={API_URL + "img/champion/" + champ.image.full}
+            />
+          ))}
         </div>
       </div>
     </>
